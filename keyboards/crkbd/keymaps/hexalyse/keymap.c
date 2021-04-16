82543449240f857bd70c065ccf3e25b5458c28fb
Hexalyse <reversebob@gmail.com>
2021-04-16 13:49:46 +0200

    Modified ergodash and crkbd personal keymaps

--- keyboards/crkbd/keymaps/hexalyse/keymap.c
+++ keyboards/crkbd/keymaps/hexalyse/keymap.c
@@ -93,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         RESET,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F8, KC_F8, KC_F9, KC_F10, KC_F11,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGBRST, XXXXXXX,                      XXXXXXX, TOGGLE_LEFT_OLED, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,\
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, TOGGLE_LEFT_OLED, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGBRST, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
                                       //`--------------------------'  `--------------------------'
@@ -163,12 +163,13 @@ void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
     matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, "");
     matrix_write_ln(matrix, read_keylog());
     //matrix_write_ln(matrix, read_keylogs());
     //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
     //matrix_write_ln(matrix, read_host_led_state());
     //matrix_write_ln(matrix, read_timelog());
-    matrix_write_ln(matrix, read_rgb_info());
+    //matrix_write_ln(matrix, read_rgb_info());
     //matrix_write_ln(matrix, "");
     sprintf(wpm_str, "WPM: %03d", get_current_wpm());
     matrix_write(matrix, wpm_str);
